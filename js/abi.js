3a7aad55c581598ec15c49aa9e51a5546f5c0aea
0xGold Token <39569021+0xGold@users.noreply.github.com>
2018-08-27 13:40:11 -0400

    Update abi.js

--- js/abi.js
+++ js/abi.js
@@ -1,6 +1,8 @@
-// '0xBitcoin Token' contract ABI
+// '0xGold Token' contract ABI
 //
-// address: 0xB6eD7644C69416d67B522e20bC294A9a9B405B31
+// address: 0x291de53a16b76dfe28551fd3335225f506db8b82
+//
+// still need to add 
 //
 const tokenABI = [
    {
